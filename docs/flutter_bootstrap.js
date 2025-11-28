71645f843834936d9d2d7c7fc983b7370eae422d
Vikrm Verma <vikrm@saffronbolt.in>
2025-11-28 13:24:54 +0530

    Update Flutter web build for Cloudflare

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "736703134"
+    serviceWorkerVersion: "2510359704"
   }
 });
