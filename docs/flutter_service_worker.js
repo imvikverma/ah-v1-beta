36c9217ef06e916c107aafae89eb1ffae3b7efe9
GitHub Action <action@github.com>
2025-11-29 16:19:57 +0000

    Auto-deploy: Update Flutter web build (fallback)

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "3bfc2a9595a690a0250a978e389804cc",
+"flutter_bootstrap.js": "42a0383833a1044ccae3f9fd294ecc42",
 "version.json": "a742cecd643c70862af54c499df67187",
 "main.dart.js": "7c784738bf8605aae996cd1a841eb5f4"};
 // The application shell files that are downloaded before a service worker can
