871d63a0a46114279ab0488f2390b98915124c1c
Vikrm Verma <vikrm@saffronbolt.in>
2025-12-06 00:37:22 +0530

    Update: Dynamic changelog and README generation system; Automated deployment with changelog-based commit messages; Deploy script now reads from CHANGELOG.md for commit messages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba600f8a2032c60d2df2312fbe62353d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "3bc5bacc2d9a3412aac2a2cd1f23a107",
+"flutter_bootstrap.js": "ae95c242f38ea6cb8971231764c852e8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
