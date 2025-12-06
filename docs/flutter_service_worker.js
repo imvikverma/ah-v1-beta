d07be38e4f99ba7aedd2c99fa737ab4d51581a41
Vikrm Verma <vikrm@saffronbolt.in>
2025-12-06 15:37:43 +0530

    Update: Light and Dark Mode theme system with theme toggle button; Logo integration on login screen (replaces AurumHarmony text); Theme-aware color system throughout the app

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba600f8a2032c60d2df2312fbe62353d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "fa4e071eabd02c0dfe891609c65201f1",
+"flutter_bootstrap.js": "76487a62a85dcf0de27a3bd5866f9ef9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
