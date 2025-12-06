6119803db7121ae4c4fca441c8934e69ff62c1f9
Vikrm Verma <vikrm@saffronbolt.in>
2025-12-06 15:17:03 +0530

    Update: Light and Dark Mode theme system with theme toggle button; Logo integration on login screen (replaces AurumHarmony text); Theme-aware color system throughout the app

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,9 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "1ec71e5cd5eea12245523c76b96fd01c",
-"assets/AssetManifest.bin.json": "2a4c49c95d52f2f8f8fce202845cad06",
+const RESOURCES = {"assets/AssetManifest.bin": "ba600f8a2032c60d2df2312fbe62353d",
+"assets/AssetManifest.bin.json": "15c0f2fe481f22a2b0571f40758df716",
 "assets/assets/logo/AurumHarmony_logo.png": "0e49a3ab338a6e13b01fd2891467b305",
+"assets/assets/logo/aurumharmony_logo.svg": "5a01e494ff62f8b4b4628e92e3c9aba8",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "6cebcdd8b934c80549f3e8e35c9f4d42",
 "assets/NOTICES": "0a4492cf71d8c0c38794baaf1ae23bec",
@@ -25,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "1ec71e5cd5eea12245523c76b96fd01c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "d3119e6e7ee2b62669136883d16db72e",
+"flutter_bootstrap.js": "7dd238112b2d75ce717d12247b9298e1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "0340d4cb80159287212c962237f55649",
 "/": "0340d4cb80159287212c962237f55649",
-"main.dart.js": "13e68ab6058364564602608657362f43",
+"main.dart.js": "8ec70fbeb51490ccdd01012c53c09beb",
 "manifest.json": "99e22d8000ed8c89a83aa6b5827ed99c",
 "version.json": "a742cecd643c70862af54c499df67187"};
 // The application shell files that are downloaded before a service worker can
