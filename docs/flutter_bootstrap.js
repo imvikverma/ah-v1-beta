923932012ed89c927a11193404543ae5d3f90ca7
GitHub Action <action@github.com>
2025-12-06 10:11:48 +0000

    Auto-deploy: Update Flutter web build

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "196414344"
+    serviceWorkerVersion: "3914720901"
   }
 });
