9dafa59b0f0325dc2adc4e961b220ac4d1e40318
Vikrm Verma <vikrm@saffronbolt.in>
2025-12-09 16:35:33 +0530

    Deploy: 2025-12-09 16:33:33

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba600f8a2032c60d2df2312fbe62353d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "02e5925caed55d1daca4b4277c8b5362",
+"flutter_bootstrap.js": "ba55081df9092c09ee7b1391bd2b2d3f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
