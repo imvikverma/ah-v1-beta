0351850a7dbc8ae8bffc517a4e3df7110adcbde0
Vikrm Verma <vikrm@saffronbolt.in>
2025-12-09 16:30:24 +0530

    Update: **Auto-Deploy System**; File watcher script (`watch_and_deploy.ps1`) that monitors Flutter frontend files; Auto-deploys to GitHub & Cloudflare when files are saved in Cursor

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1097936955"
+    serviceWorkerVersion: "3439983294"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba600f8a2032c60d2df2312fbe62353d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "e3d12c7c1bde5077223ad6809b7f3485",
+"flutter_bootstrap.js": "02e5925caed55d1daca4b4277c8b5362",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
