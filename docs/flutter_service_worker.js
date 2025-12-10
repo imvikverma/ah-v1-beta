17c0258f5c732895b333eec99a9c4333fecb591f
Vikrm Verma <vikrm@saffronbolt.in>
2025-12-10 23:23:29 +0530

    Quick deploy: Update Flutter web build (2025-12-10 23:23:29)

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba600f8a2032c60d2df2312fbe62353d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "bc4cfec899b1a6e38b048ac1a91a8d2d",
+"flutter_bootstrap.js": "c7a2302796e04c200a7e1a3a834a9d9b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
