1ff0d061f11f22035a2ce5f231c7fe38fe4c9baf
Vikrm Verma <vikrm@saffronbolt.in>
2025-12-13 19:52:48 +0530

    Deploy: JWT fixes + 10-second delay fix (2025-12-13 19:52:48)

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba600f8a2032c60d2df2312fbe62353d
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "e6350c3fd8eb4581cbff48b9033361c3",
+"flutter_bootstrap.js": "2810923e2558d6c7178a2598e49627f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
